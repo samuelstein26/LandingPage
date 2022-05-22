5ae04cd14b4fd6f97e0efde42b7572a6465e1d7b
samuelstein26 <samuel.stein@hotmail.com>
2022-05-22 10:18:55 +0100

    version 1 - initial backup

---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {scrollFunction()};
+
+/*function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    
+    
+    
+    $("#navbar").slideToggle("slow");
+  } else {
+    
+    
+  }
+}*/
