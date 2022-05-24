ab0762e5f18ca92c2ab82da1705e4e0255768ace
samuelstein26 <samuel.stein@hotmail.com>
2022-05-24 08:04:27 +0100

    fixed collapse and organized code

--- js/collapse.js
+++ js/collapse.js
@@ -1,9 +1,11 @@
-function toggleIcon(e) {
-    $(e.target)
-        .prev('.panel-heading')
-        .find(".more-less")
-        .toggleClass('glyphicon-plus glyphicon-minus');
-}
+$(document).ready(function() {
+    function toggleIcon(e) {
+        $(e.target)
+            .prev('.panel-heading')
+            .find(".more-less")
+            .toggleClass('glyphicon-plus glyphicon-minus');
+    }
 
-$('.panel-group').on('hidden.bs.collapse', toggleIcon);
-$('.panel-group').on('shown.bs.collapse', toggleIcon);
+    $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+    $('.panel-group').on('shown.bs.collapse', toggleIcon);
+});
